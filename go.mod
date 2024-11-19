89b87e4e05cb0f3ac886341de9b5cdbe24afc132
João Victor <joaovictorvvm@edu.unifor.br>
2024-11-19 16:00:21 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module back
+
+go 1.23.1
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/nedpals/supabase-go v0.4.0
+)
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/nedpals/postgrest-go v0.1.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	supa "github.com/nedpals/supabase-go"
+)
+
+type ProductNoID struct {
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+}
+
+type Product struct {
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+}
+
+var (
+	supabaseUrl string
+	supabaseKey string
+	port        string
+	supabase    *supa.Client
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	supabaseUrl = os.Getenv("SUPA_URL")
+	supabaseKey = os.Getenv("SUPA_KEY")
+	supabase = supa.CreateClient(supabaseUrl, supabaseKey)
+
+	port = os.Getenv("PORT")
+}
+
+func AllProductsHandler(w http.ResponseWriter, r *http.Request) {
+	var results []Product
+	err := supabase.DB.From("products").Select("*").Execute(&results)
+	if err != nil {
+		w.WriteHeader(400)
+		fmt.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(results); err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		log.Printf("Error encoding JSON: %v", err)
+	}
+}
+
+func NewProductHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	descriptionStr := r.PathValue("description")
+	description := descriptionStr[2:] // Remove "d=" from description
+	priceStr := r.PathValue("price")
+	price, _ := strconv.ParseFloat(priceStr, 64)
+
+	p := ProductNoID{name, description, price}
+
+	var product []Product
+	err := supabase.DB.From("products").Insert(p).Execute(&product)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(product); err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		log.Printf("Error encoding JSON: %v", err)
+	}
+}
+
+func DeleteProductHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	var products []Product
+	err := supabase.DB.From("products").Delete().Eq("id", id).Execute(&products)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Deleted: %v\n", products)
+	w.WriteHeader(200)
+}
+
+func EditProductHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
+	id, _ := strconv.Atoi(idStr)
+	name := r.PathValue("name")
+	descriptionStr := r.PathValue("description")
+
+	var description string
+	_, err := fmt.Sscanf(descriptionStr, "d=%s", &description)
+	if err != nil {
+		panic(err)
+	}
+
+	priceStr := r.PathValue("price")
+	price, _ := strconv.ParseFloat(priceStr, 64)
+
+	p := Product{id, name, description, price}
+
+	var results []Product
+	err = supabase.DB.From("products").Update(p).Eq("id", idStr).Execute(&results)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Updated: %v\n", results)
+	w.WriteHeader(200)
+}
+
+func main() {
+	http.HandleFunc("/products/", AllProductsHandler)
+	http.HandleFunc("/new/{name}/{description}/{price}", NewProductHandler)
+	http.HandleFunc("/delete/{id}", DeleteProductHandler)
+	http.HandleFunc("/edit/{id}/{name}/{description}/{price}", EditProductHandler)
+
+	http.ListenAndServe(port, nil)
+}
